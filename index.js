e6a935fa950c5228a8be7f3da1d2a2f655c170de
Ankit8125 <117138095+Ankit8125@users.noreply.github.com>
2024-03-19 13:57:10 +0530

    added initial files

---
+++ index.js
@@ -0,0 +1,8 @@
+const clock = document.getElementById('clock');
+// const clock = document.querySelector('#clock')
+
+setInterval(function () {
+  let date = new Date();
+  // console.log(date.toLocaleTimeString());
+  clock.innerHTML = date.toLocaleTimeString();
+}, 1000); // 1000ms = 1s    
